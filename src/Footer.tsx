ef728878cfe7930021e72bce6a3c0a770e36470b
unknown <easycom2011@gmail.com>
2026-01-14 14:09:18 +0100

    updated footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,12 +9,12 @@ export function Footer() {
                <h1><span className="logo-zen">ZEN</span><span className="logo-ma">MA</span></h1>
               </a>
 
-              <span className="footer__copyright">© ZENMA, 2019—2024 <br /> Create by <a href="https://themeforest.net/user/dmitryvolkov/portfolio" target="_blank">Dmitry Volkov</a></span>
+              <span className="footer__copyright">© ZENMA, {new Date().getFullYear()} <br /> </span>
 
               <nav className="footer__nav">
-                <a href="/aboutus">About Us</a>
-                <a href="/contacts">Contacts</a>
-                <a href="/privacy">Privacy policy</a>
+                {/* <a href="/aboutus">About Us</a> */}
+                {/* <a href="/contacts">Contacts</a> */}
+
               </nav>
 
               <button className="footer__back" type="button">
